ed5e168075e40310a57d0a8f1b489a1d3abc6668
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-23 13:33:19 +0530

    fix: for image

--- components/FoodCard.jsx
+++ components/FoodCard.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const FoodCard = () => {
   return (
-    <div className='bg-gray-900 h-[30vh] w-[200px] rounded'>
-        
+    <div className="rounded flex flex-col items-center  w-[200px] max-h-[40%] shadow-md overflow-hidden h-50vh  ">
+      <div className="relative h-[150px] w-[200px] ">
+        <Image
+          src="/pasta-con-salsa-cremosa-de-tomate-1.jpg"
+          alt="Delicious food"
+          fill
+          className="object-cover"
+          sizes="200px"
+        />
+        <div className="h-5">
+          <h1>Pasta</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodCard
+export default FoodCard;
