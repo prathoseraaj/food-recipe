6838b6d20e5aea53584a127ae3670710b6ec1d56
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-23 12:51:53 +0530

    feat: component foodcard

---
+++ components/FoodCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FoodCard = () => {
+  return (
+    <div className='bg-gray-900 h-[30vh] w-[200px] rounded'>
+        
+    </div>
+  )
+}
+
+export default FoodCard
