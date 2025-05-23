32080ce7a8b830cfc150d7df40067659820342b1
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-23 13:43:17 +0530

    fix: food name

--- components/FoodCard.jsx
+++ components/FoodCard.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const FoodCard = () => {
   return (
-    <div className="rounded flex flex-col items-center  w-[200px] max-h-[40%] shadow-md overflow-hidden h-50vh  ">
-      <div className="relative h-[150px] w-[200px] ">
+    <div className="rounded flex flex-col items-center  w-[225px] max-h-[50%] shadow-md overflow-hidden h-50vh  ">
+      <div className="relative h-[250px] w-[225px] ">
         <Image
           src="/pasta-con-salsa-cremosa-de-tomate-1.jpg"
           alt="Delicious food"
@@ -12,10 +12,10 @@ const FoodCard = () => {
           className="object-cover"
           sizes="200px"
         />
-        <div className="h-5">
-          <h1>Pasta</h1>
-        </div>
       </div>
+      <div className=" flex flex-row gap-5 ">
+          <h1 className="font-bold text-style: italic text-[20px] ">Pasta</h1>
+        </div>
     </div>
   );
 };
