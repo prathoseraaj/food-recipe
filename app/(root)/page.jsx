02a0e1781d391aa3ba1b157e762993470911a3c6
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-23 12:51:39 +0530

    feat: root

---
+++ app/(root)/page.jsx
@@ -0,0 +1,12 @@
+import FoodCard from '@/components/FoodCard'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex h-[90vh] w-[100%] flex pl-10 pt-10  '>
+        <FoodCard/>
+    </div>
+  )
+}
+
+export default page
