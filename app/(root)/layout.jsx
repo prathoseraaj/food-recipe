a93b299fd7edc6d9ea9fd05a1a8ba494890d11ab
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-23 12:51:28 +0530

    feat: background

---
+++ app/(root)/layout.jsx
@@ -0,0 +1,16 @@
+import React, {  ReactNode } from "react";
+
+const layout = ({children}) => {
+  return (
+    <>
+    <div>
+    <nav className=' w-[100%] h-[7vh] flex items-center pl-10'>
+            <h1 className='font-bold text-[20px] font-style: italic'>Food-Recipe</h1>
+        </nav>
+      {children}
+    </div>
+    </>
+  );
+};
+
+export default layout;
