e139e07647c8fcc9a3fab93d9107d528ee5381e7
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-24 18:49:20 +0530

    fix: as for catagory foods

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,12 +2,29 @@
 
 import FoodCard from "@/components/FoodCard";
 import React, { useEffect, useState } from "react";
-import { fetchcategory } from "@/services/api";
+import { fetchcategory, fetchfoods } from "@/services/api";
 
 const page = () => {
+  const [selectedCatagory, setSelectedCatagory] = useState(null);
+  const [catagoryfoods, setCatagoryfoods] = useState([]);
   const [catagories, setCatagories] = useState([]);
   const [loading, setLoading] = useState(false);
   console.log(catagories);
+  console.log(catagoryfoods);
+
+  const handleCatagory = async(catagoryName) =>{
+    setLoading(true);
+    try{
+      const fooddata = await fetchfoods(catagoryName);
+      setCatagoryfoods(fooddata);
+      setSelectedCatagory(catagoryName);
+    }
+    catch(error){
+      console.log("Error in fetching", error);
+    }finally{
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
     const loadingcatagories = async () => {
@@ -29,10 +46,12 @@ const page = () => {
     <div className="flex h-[90vh] w-[100%] flex pl-10 pt-10  ">
       {loading ? (
         <div>loading</div>
-      ) : (
+      ) : selectedCatagory? (<div>
+       <>catagory code</>
+      </div>):(
         <div className="flex flex-wrap gap-5">
           {catagories.length > 0 ? ( catagories.map((catagory, index) => (
-            <FoodCard key={index} catagory={catagory}/>
+            <FoodCard key={index} catagory={catagory} handleCatagory={handleCatagory}/>
           ))) : <div>Error! Catogories not found</div>}
         </div>
       )}
--- components/FoodCard.jsx
+++ components/FoodCard.jsx
@@ -1,11 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-const FoodCard = ({catagory}) => {
-
-  const handleCatagory = (catagoryfood) =>{
-    console.log(catagoryfood);
-  }
+const FoodCard = ({catagory, handleCatagory}) => {
 
   return (
     <div className="rounded flex flex-col items-center  w-[225px] max-h-[50%] shadow-md overflow-hidden h-50vh  ">
