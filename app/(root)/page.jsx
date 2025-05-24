51afa98e825a3125df9ad71633655ce98eb841d7
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-24 12:17:44 +0530

    fix: useeffect

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,14 +1,36 @@
-import FoodCard from '@/components/FoodCard'
-import React, { useState } from 'react'
+"use client"
+
+import FoodCard from "@/components/FoodCard";
+import React, { useEffect, useState } from "react";
+import { fetchcategory } from "@/services/api";
 
 const page = () => {
   const [catagories, setCatagories] = useState([]);
-  const [loading, setLoading] = useState(flase);
-  return (
-    <div className='flex h-[90vh] w-[100%] flex pl-10 pt-10  '>
-        <FoodCard/>
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  console.log(catagories);
 
-export default page
+  useEffect(() => {
+    const loadingcatagories = async () => {
+      setLoading(true);
+      try {
+        const catagorydata = await fetchcategory();
+        setCatagories(catagorydata);
+      } catch (error) {
+        console.log("Error fetcning the catagory,", error);
+      }finally{
+        setLoading(flase);
+      }
+    };
+
+    loadingcatagories();
+
+  }, []);
+
+  return (
+    <div className="flex h-[90vh] w-[100%] flex pl-10 pt-10  ">
+      <FoodCard />
+    </div>
+  );
+};
+
+export default page;
