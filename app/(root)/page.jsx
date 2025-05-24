f63cda59ba63c9d119dc9bd2db498581a70b4927
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-24 12:47:27 +0530

    fix: typo as error

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import FoodCard from "@/components/FoodCard";
 import React, { useEffect, useState } from "react";
@@ -17,18 +17,25 @@ const page = () => {
         setCatagories(catagorydata);
       } catch (error) {
         console.log("Error fetcning the catagory,", error);
-      }finally{
-        setLoading(flase);
+      } finally {
+        setLoading(false);
       }
     };
 
     loadingcatagories();
-
   }, []);
 
   return (
     <div className="flex h-[90vh] w-[100%] flex pl-10 pt-10  ">
-      <FoodCard />
+      {loading ? (
+        <div>loading</div>
+      ) : (
+        <div className="flex flex-wrap">
+          {catagories.length > 0 ? ( catagories.map((catagory, index) => (
+            <FoodCard key={index} catagory={catagory}/>
+          ))) : <div>Error! Catogories not found</div>}
+        </div>
+      )}
     </div>
   );
 };
--- components/FoodCard.jsx
+++ components/FoodCard.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import React from "react";
 
-const FoodCard = () => {
+const FoodCard = ({catagory}) => {
   return (
     <div className="rounded flex flex-col items-center  w-[225px] max-h-[50%] shadow-md overflow-hidden h-50vh  ">
       <div className="relative h-[250px] w-[225px] ">
         <Image
-          src="/pasta-con-salsa-cremosa-de-tomate-1.jpg"
+          src={catagory.strCategoryThumb}
           alt="Delicious food"
           fill
           className="object-cover"
@@ -14,8 +14,9 @@ const FoodCard = () => {
         />
       </div>
       <div className=" flex flex-row gap-5 ">
-          <h1 className="font-bold text-style: italic text-[20px] ">Pasta</h1>
+          <h1 className="font-bold text-style: italic text-[20px] ">{catagory.strCategory}</h1>
         </div>
+
     </div>
   );
 };
