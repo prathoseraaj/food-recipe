e0597e4f4dd051b42b27951482810e20500c8faf
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-24 18:19:31 +0530

    feat: to render the name of the catagory

--- components/FoodCard.jsx
+++ components/FoodCard.jsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import React from "react";
 
 const FoodCard = ({catagory}) => {
+
+  const handleCatagory = (catagoryfood) =>{
+    console.log(catagoryfood);
+  }
+
   return (
     <div className="rounded flex flex-col items-center  w-[225px] max-h-[50%] shadow-md overflow-hidden h-50vh  ">
       <div className="relative h-[150px] w-[200px] ">
@@ -14,7 +19,7 @@ const FoodCard = ({catagory}) => {
         />
       </div>
       <div className=" flex flex-row gap-5 ">
-          <h1 className="font-bold text-style: italic text-[20px] ">{catagory.strCategory}</h1>
+          <h1 className="font-bold text-style: italic text-[20px] " onClick={()=>handleCatagory(catagory.strCategory)}>{catagory.strCategory}</h1>
         </div>
 
     </div>
