d627825f7417a74d986c394583681883784110e0
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-24 12:03:55 +0530

    fix: get only the catogories

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,7 +1,9 @@
 import FoodCard from '@/components/FoodCard'
-import React from 'react'
+import React, { useState } from 'react'
 
 const page = () => {
+  const [catagories, setCatagories] = useState([]);
+  const [loading, setLoading] = useState(flase);
   return (
     <div className='flex h-[90vh] w-[100%] flex pl-10 pt-10  '>
         <FoodCard/>
--- services/api.js
+++ services/api.js
@@ -1,7 +1,8 @@
 const fetchcategory = async() =>{
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
     const data = await response.json();
-    console.log(data);
+    console.log(data.categories);
+    return data.categories;
 }
 
 fetchcategory();
