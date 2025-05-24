872d25e282fbaa384353a9c6fae8de9e8d364a85
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-24 12:17:37 +0530

    fix: to export

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
-const fetchcategory = async() =>{
+export const fetchcategory = async() =>{
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
     const data = await response.json();
-    console.log(data.categories);
+    //console.log(data.categories);
     return data.categories;
 }
 
