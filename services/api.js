95c3cc6c3d2de21ca2553a0b4fdae617197c3658
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-24 18:05:15 +0530

    feat: fetch food recipe

--- services/api.js
+++ services/api.js
@@ -5,4 +5,11 @@ export const fetchcategory = async() =>{
     return data.categories;
 }
 
-fetchcategory();
+export const fetchfoodrecipe = async (query) =>{
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+fetchfoodrecipe("biryani");
