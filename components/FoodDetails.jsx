fad841e2503fcbdd7c8fb11499dc6e01afb64485
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-26 20:51:57 +0530

    feat: api rendering for the details

--- components/FoodDetails.jsx
+++ components/FoodDetails.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import React from "react";
 
-const FoodDetails = () => {
+const FoodDetails = ({mealDetails}) => {
   return (
-    <div className=" w-full  ">
+    <div className=" w-full p-6 -mt-10 ">
       <div className="mt-10 ml-10 flex flex-col  ">
         <div className="flex items-center flex-col">
-          <h1 className="text-[30px] text-style: italic ">Food Name</h1>
+          <h1 className="text-[30px] text-style: italic ">{mealDetails.strMeal}</h1>
           <div className="-mt-2 flex flex-row gap-5 ml-6">
-            <p>Area</p>
-            <p>Catagory</p>
+            <p>{mealDetails.strArea}</p>
+            <p>{mealDetails.strCategory}</p>
           </div>
         </div>
         <div className="flex flex-row gap-10 mt-5 flex justify-center">
@@ -17,28 +17,28 @@ const FoodDetails = () => {
                 <div className="mt-5 ml-5">
                     <h1 className="text-[17px]">Ingredients:</h1>
                     <div>
-                      <li>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
-                      </li>
+                      <ul>
+                        <li>{mealDetails.strIngredient1}</li>
+                        <li>{mealDetails.strIngredient2}</li>
+                        <li>{mealDetails.strIngredient3}</li>
+                        <li>{mealDetails.strIngredient4}</li>
+                        <li>{mealDetails.strIngredient5}</li>
+                        <li>{mealDetails.strIngredient6}</li>
+                        <li>{mealDetails.strIngredient7}</li>
+                        <li>{mealDetails.strIngredient8}</li>
+                        <li>{mealDetails.strIngredient9}</li>
+                        <li>{mealDetails.strIngredient10}</li>
+                        <li>{mealDetails.strIngredient11}</li>
+                        <li>{mealDetails.strIngredient12}</li>
+                        <li>{mealDetails.strIngredient13}</li>
+                        <li>{mealDetails.strIngredient14}</li>
+                        <li>{mealDetails.strIngredient15}</li>
+                        <li>{mealDetails.strIngredient16}</li>
+                        <li>{mealDetails.strIngredient17}</li>
+                        <li>{mealDetails.strIngredient18}</li>
+                        <li>{mealDetails.strIngredient19}</li>
+                        <li>{mealDetails.strIngredient20}</li>
+                      </ul>
                     </div>
                 </div>
             </div>
@@ -47,16 +47,16 @@ const FoodDetails = () => {
                     <h1 className="text-[17px]">Instruction:</h1>
                     <div>
                       <p>
-                        
+                        {mealDetails.strInstructions}
                       </p>
                     </div>
                 </div>
             </div>
             <div className="relative h-[300px] rounded-[10px] w-[25%] bg-white shadow-lg">
-                <Image src="https://www.themealdb.com/images/media/meals/xrttsx1487339558.jpg"
+                <Image src={mealDetails.strMealThumb}
                 alt="image"
                 fill
-                className="object-cover rounded-[10px]">
+                className="  rounded-[10px]">
 
                 </Image>
             </div>
