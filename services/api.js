d14f3ae7969cc171dbe28eae8f91989249a93bab
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-26 20:50:41 +0530

    fix: api for to get object

--- services/api.js
+++ services/api.js
@@ -16,5 +16,5 @@ export const fetchfoodrecipe = async (mealName) =>{
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`);
     const data = await response.json();
     console.log(data.meals);
-    return data.meals;
+    return data.meals[0];
 }
