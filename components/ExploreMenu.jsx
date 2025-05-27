08f808f1bf05637ead44c8188878c7c030e42d78
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-27 22:14:26 +0530

    fix: imported font ubantu and outfit

--- components/ExploreMenu.jsx
+++ components/ExploreMenu.jsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const ExploreMenu = () => {
   return (
-    <div className='bg-[url("https://miro.medium.com/v2/da:true/resize:fit:1200/0*Q8eVziOODJ75SkYb")] mb-10 w-100% h-[300px] rounded-[20px] ml-20 mr-20  bg-cover bg-center flex items-center text-white'>
+    <div className='bg-[url("https://miro.medium.com/v2/da:true/resize:fit:1200/0*Q8eVziOODJ75SkYb")] mb-10 w-100% h-[400px] rounded-[20px] ml-20 mr-20 bg-cover bg-center flex items-center text-white'>
+  <h1 className="ml-10 text-[40px]  font-outfit">Find Your Favuroite Recipe Here</h1>
 </div>
   )
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      },
     },
   },
   plugins: [],
