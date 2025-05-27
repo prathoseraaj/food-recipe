3d9f20f90593b2d5dabe17d15c83e6dfae1bbcc4
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-27 21:59:47 +0530

    feat: explore menu header

---
+++ components/ExploreMenu.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import React from "react";
+
+const ExploreMenu = () => {
+  return (
+    <div className='bg-[url("https://miro.medium.com/v2/da:true/resize:fit:1200/0*Q8eVziOODJ75SkYb")] mb-10 w-100% h-[300px] rounded-[20px] ml-20 mr-20  bg-cover bg-center flex items-center text-white'>
+</div>
+  )
+};
+
+export default ExploreMenu;
