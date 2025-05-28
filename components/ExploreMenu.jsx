560aad46c6533b56d4cef2194e09ce2032a48504
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-28 11:21:14 +0530

    fix: image

--- components/ExploreMenu.jsx
+++ components/ExploreMenu.jsx
@@ -3,10 +3,14 @@ import React from "react";
 
 const ExploreMenu = () => {
   return (
-    <div className='bg-[url("https://miro.medium.com/v2/da:true/resize:fit:1200/0*Q8eVziOODJ75SkYb")] mb-10 w-100% h-[400px] rounded-[20px] ml-20 mr-20 bg-cover bg-center flex items-center text-white'>
-  <h1 className="ml-10 text-[40px] ">Find Your Favuroite Recipe Here</h1>
-</div>
-  )
+    <div className='bg-[url("https://png.pngtree.com/background/20230517/original/pngtree-person-cooking-at-the-kitchen-counter-picture-image_2625819.jpg")] mb-10 w-100% h-[400px] rounded-[20px] ml-20 mr-20 bg-cover bg-center flex items-center text-white'>
+      <div>
+        <h1 className="ml-10 text-[40px] mt-10 ">
+          Find Your Favuroite Recipe Here
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default ExploreMenu;
