c5fe86f13555aea4ad5f6cb50d20510804158639
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-29 15:30:29 +0530

    fix: food detail to open in new page

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -63,41 +63,46 @@ const page = () => {
 
   return (
     <div className="flex max-h-200vh mb-10 w-full flex-col font-outfit ">
-      <ExploreMenu />
-      <div className="flex overflow-x-scroll no-scrollbar gap-5 ml-20 mr-20">
-        {catagories.length > 0 ? (
-          catagories.map((catagory, index) => (
-            <FoodCard
-              key={index}
-              catagory={catagory}
-              handleCatagory={handleCatagory}
-            />
-          ))
-        ) : (
-          <div>Error! Catogories not found</div>
-        )}
-      </div>
-      {loading ? (
-        <div className="ml-20">loading</div>
-      ) : selectedMeal ? (
+      {selectedMeal ? (
         <FoodDetails mealDetails={mealDetails} />
-      ) : selectedCatagory ? (
-        <div className="mt-10 ml-20 ">
-          <h1 className="text-[20px] mb-5 ">
-            The food based on the selected catagory are:
-          </h1>
-          <div className="flex flex-wrap gap-5 ">
-            {catagoryfoods.length > 0 ? (
-              catagoryfoods.map((meal, index) => (
-                <FoodCard key={index} meal={meal} handleMeal={handleMeal} />
+      ) : (
+        <div>
+          {" "}
+          <ExploreMenu />
+          <div className="flex overflow-x-scroll no-scrollbar gap-5 ml-20 mr-20">
+            {catagories.length > 0 ? (
+              catagories.map((catagory, index) => (
+                <FoodCard
+                  key={index}
+                  catagory={catagory}
+                  handleCatagory={handleCatagory}
+                />
               ))
             ) : (
-              <div>Error! Catogories foods not found</div>
+              <div>Error! Catogories not found</div>
             )}
           </div>
+          {loading ? (
+            <div className="ml-20">loading</div>
+          ) : selectedCatagory ? (
+            <div className="mt-10 ml-20 ">
+              <h1 className="text-[20px] mb-5 ">
+                The food based on the selected catagory are:
+              </h1>
+              <div className="flex flex-wrap gap-5 ">
+                {catagoryfoods.length > 0 ? (
+                  catagoryfoods.map((meal, index) => (
+                    <FoodCard key={index} meal={meal} handleMeal={handleMeal} />
+                  ))
+                ) : (
+                  <div>Error! Catogories foods not found</div>
+                )}
+              </div>
+            </div>
+          ) : (
+            <div className="ml-20 mt-10">select the catagory</div>
+          )}
         </div>
-      ) : (
-        <div className="ml-20 mt-10">select the catagory</div>
       )}
     </div>
   );
