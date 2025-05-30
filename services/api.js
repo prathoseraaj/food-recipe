6f0bae82fd0f814a4b6ebf69448dfa9997143dc4
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-30 20:14:22 +0530

    fix: comment

--- services/api.js
+++ services/api.js
@@ -8,13 +8,14 @@ export const fetchcategory = async() =>{
 export const fetchfoods = async (catagoryName) => {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${catagoryName}`);
     const data = await response.json();
-    console.log(data.meals);
+    //console.log(data.meals);
     return data.meals;
 }
 
 export const fetchfoodrecipe = async (mealName) =>{
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`);
     const data = await response.json();
-    console.log(data.meals);
+    //console.log(data.meals); 
     return data.meals[0];
 }
+
