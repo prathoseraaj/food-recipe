1157d9e28b46de7e6872a9ef6f4c72b0b2aca269
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-30 20:12:38 +0530

    fix: mb

--- components/ExploreMenu.jsx
+++ components/ExploreMenu.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ExploreMenu = () => {
   return (
-    <div className='bg-[url("https://png.pngtree.com/background/20230517/original/pngtree-person-cooking-at-the-kitchen-counter-picture-image_2625819.jpg")] mb-10 w-100% h-[400px] rounded-[20px] ml-20 mr-20 bg-cover bg-center flex items-center text-white'>
+    <div className='bg-[url("https://png.pngtree.com/background/20230517/original/pngtree-person-cooking-at-the-kitchen-counter-picture-image_2625819.jpg")] mb-10 w-100% h-[400px] rounded-[20px] ml-20  mt-10 mr-20 bg-cover bg-center flex items-center text-white'>
       <div className="flex flex-col">
         <h1 className="ml-10 text-[40px] mt-10 ">
           Find Your Favuroite Recipe Here
