2ab35b55e02f9ccb07daf65df98e9bfd34b0ac1f
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-05-30 19:25:27 +0530

    fix: background

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,14 +1,14 @@
 const Layout = ({ children }) => {
   return (
     <>
-      <nav className="w-full h-[7vh] flex items-center pl-10 mb-5 text-white bg-blue-900">
+      <nav className="w-full h-[7vh] flex items-center pl-10 mb-5 text-white bg-black">
         <h1 className="font-bold text-[20px]">Food-Recipe</h1>
       </nav>
 
       <main className="flex-grow">{children}</main>
 
-      <footer className="bg-black text-white w-full p-4 flex justify-center ">
-        <h1>Food Recipe</h1>
+      <footer className="bg-black text-white w-full p-4 flex justify-center mt-20 ">
+        <h1>©Food Recipe . All rights reserved</h1>
       </footer>
     </>
   );
